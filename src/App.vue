2f1bd9db80287e8abbc6e7da54eed48fae53f6e5
milley <69milley@gmail.com>
2022-11-14 02:07:17 +0500

    + style

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card">
+    <h1>{{ titleH }}</h1>
+    <div class="form-control">
+      <input
+          type="text"
+          v-model="todo"
+          :maxlength="maxInput"
+          placeholder="Введите вашу задачу..." />
+
+      <button class="btn" @click="addTodo(id++)" >Добавить</button>
+
+    </div>
+    <ul class="list" v-for="(todo, i) in todos" :key="todo.id">
+
+      <li :class="{ done: todo.isCompleted }" @dblclick="removeTodo(i)"> <span>{{ i + 1 }}. {{ todo.text }}</span>
+        <input v-model="todo.isCompleted" type="checkbox">
+
+      </li>
+    </ul>
+    <hr />
+    <p class="primary">Всего задач: {{ todos.length }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+
+  data(){
+    return {
+      titleH: 'Мой Todo лист',
+      todo: '',
+      maxInput: 25,
+      isDone: false,
+      id: 0,
+      // todos: [{ "id": 1, "text": "qwe", "isCompleted": false }, { "id": 2, "text": "qwesss", "isCompleted": false }]
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push ({
+        id: this.id,
+        text: this.todo,
+        isCompleted: this.isDone
+      });
+      this.todo = '';
+    },
+    removeTodo(index){
+      this.todos.splice(index , 1)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../styles/theme.css";
+@import "../styles/main.css";
+
 </style>
